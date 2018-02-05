d5a81e23c3aef9d4f667e4b19a5da190f9afc9a7
mia <c.napaswan@gmail.com>
2018-02-06 10:59:16 +1100

    banana icon arrived!

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,16 @@
 class StoriesController < ApplicationController
+  def new
+  end
+
+  def create
+    story = Story.new
+    story.title = params[:title]
+    story.image_url = params[:image_url]
+    if story.save
+      redirect_to "/"
+    else
+      redirect_to 'stories/new'
+    end
+    
+  end
 end

6e3318caa0b862eebf209e72775304f2983438b7
mia <c.napaswan@gmail.com>
2018-02-06 14:45:58 +1100

    story show page able to add entries

--- app/controllers/story_entries_controller.rb
+++ app/controllers/story_entries_controller.rb
@@ -1,2 +1,12 @@
 class StoryEntriesController < ApplicationController
+  def create
+    @story_entries = StoryEntry.new
+    @story_entries.story_id = params[:story_id]
+    @story_entries.user_id = params[:user_id]
+    @story_entries.body = params[:body]
+    if @story_entries.save
+      redirect_to "/"
+    end
+  end
+
 end

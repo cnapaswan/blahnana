647d3c3cc1f12c3e168051ad079a37f7b3ea90d8
Jen <jen@Jens-MacBook-Pro.local>
2018-02-08 09:59:21 +1100

    please rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.destory_all
+Product.destroy_all
 
-Product.create(product_item:"type1", price:0.99, item_count:10)
-Product.create(product_item:"type2", price:2.99, item_count:35)
-Product.create(product_item:"type1", price:4.99, item_count:60)
+=begin
+Product.create([{product_item:"type1", price:0.99, item_count:10},
+	{product_item:"type2", price:2.99, item_count:35},
+	{product_item:"type3", price:4.99, item_count:60}])
+
+=end
+
+p4 = Product.new
+p4.product_item="type1"
+p4.price=0.99
+p4.item_count=10
+p4.save
+
+p5 = Product.new
+p5.product_item="type2"
+p5.price=2.99
+p5.item_count=35
+p5.save
+
+p6 = Product.new
+p6.product_item="type3"
+p6.price=4.99
+p6.item_count=35
+p6.save

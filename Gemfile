770f659baac29103d97bb410400f60ce4875c775
mia <c.napaswan@gmail.com>
2018-02-07 23:56:42 +1100

    add aws database/ change image url to image upload

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ git_source(:github) do |repo_name|
 end
 
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
@@ -32,7 +31,7 @@ gem 'jbuilder', '~> 2.5'
  gem 'bcrypt', '~> 3.1.7'
  
  gem 'carrierwave'
-#gem 'fog-aws'
+ gem 'fog-aws'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,22 +2,24 @@ class StoriesController < ApplicationController
 
   def show
     @story = Story.find(params[:id])
-    @story_entries = @story.story_entries
-
+    # @story_entries = @story.story_entries
+    @story_entries = StoryEntry.where(story_id: params[:id])
   end
 
-  def api_create
+  def create
     story = Story.new
     story.title = params[:title]
-    story.image_url = params[:image_url]
     story.story_img = params[:img_file]
     if story.save 
-      render json: story
+      return
     else
       render json: {error: "error is here"}
     end
   end
 
-
+  def api_show
+    story = Story.last
+    render json: story
+  end
 
 end

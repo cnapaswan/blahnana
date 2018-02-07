83689e525408141c757f4b73b723ca46eea99af4
mia <c.napaswan@gmail.com>
2018-02-08 10:37:42 +1100

    delete column image_url from stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207051020) do
+ActiveRecord::Schema.define(version: 20180207233055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20180207051020) do
   create_table "stories", force: :cascade do |t|
     t.string "title"
     t.integer "banana"
-    t.text "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "story_img"
